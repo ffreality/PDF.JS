ff4ab92d546ab821b328a8c075d8138b98f2f26a
Frozen Forest Reality Technologies <frozenforeststudio@gmail.com>
2020-08-06 03:24:32 +0300

    Update viewer.js

--- web/viewer.js
+++ web/viewer.js
@@ -1752,7 +1752,7 @@ var PDFViewerApplication = {
 exports.PDFViewerApplication = PDFViewerApplication;
 var validateFileURL;
 {
-  var HOSTED_VIEWER_ORIGINS = ['null', 'http://mozilla.github.io', 'https://mozilla.github.io'];
+  var HOSTED_VIEWER_ORIGINS = ['null', 'http://mozilla.github.io', 'https://mozilla.github.io', 'http://www.pdf995.com/'];
 
   validateFileURL = function validateFileURL(file) {
     if (file === undefined) {
